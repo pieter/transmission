934f800ead5fa5c7bc1bda81bc2e96551ed92ebb
Pieter de Bie <pdebie@ai.rug.nl>
2008-12-04 21:17:02 +0100

    Fake SVN revision information to use git revision instead

--- macosx/version.sh
+++ macosx/version.sh
@@ -10,22 +10,9 @@
 PEERID_PREFIX="-TR140Z-"
 USERAGENT_PREFIX="1.40+"
 
-SVN_REVISION=`find ./libtransmission -name "*\.[chmp]" -o -name "*\.cpp" -o -name "*\.po" -o -name "*\.sh" | \
-              xargs grep "\$Id:" | \
-              grep -v third-party | \
-              cut -d"$Id:" -f3 | cut -d" " -f3 | sort -n | tail -n 1`
+SVN_REVISION=`git rev-parse --short HEAD`
+SVN_REVISION_MAC=$SVN_REVISION
 
-#dirty fix to ensure the highest version number is found when running release_builder.sh
-SVN_REVISION_MAC=`find ./macosx -name "*\.[chmp]" -o -name "*\.cpp" -o -name "*\.po" -o -name "*\.sh" | \
-              xargs grep "\$Id:" | \
-              grep -v third-party | \
-              cut -d"$Id:" -f3 | cut -d" " -f3 | sort -n | tail -n 1`
-
-if [ $SVN_REVISION_MAC -gt $SVN_REVISION ]
-then
-	SVN_REVISION=$SVN_REVISION_MAC
-fi
-  
 # Generate files to be included: only overwrite them if changed so make
 # won't rebuild everything unless necessary
 replace_if_differs ()
